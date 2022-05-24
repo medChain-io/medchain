b5df81fb78a92e73692df48fa5b0ace2b02c879e
WillSchwieder <wschwied@chicagobooth.edu>
2022-05-24 20:38:01 +0000

    devise and first table

---
+++ app/models/medwallet_account.rb
@@ -0,0 +1,6 @@
+class MedwalletAccount < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
